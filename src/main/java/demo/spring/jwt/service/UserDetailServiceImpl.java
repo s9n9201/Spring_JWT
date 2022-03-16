3a2daa1bc17a44f0b8f3d8726df5f45bff8e776c
kent <s9n9201@yahoo.com.tw>
2022-03-16 17:54:55 +0800

    add UserDetailService and UserDetails with Implements

---
+++ src/main/java/demo/spring/jwt/service/UserDetailServiceImpl.java
@@ -0,0 +1,23 @@
+package demo.spring.jwt.service;
+
+import demo.spring.jwt.entity.User;
+import demo.spring.jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepository.findByUsername(username).orElseThrow(
+                ()->new UsernameNotFoundException("User Not Found with UserName: "+username)
+        );
+        return UserDetailsImpl.build(user);
+    }
+}
