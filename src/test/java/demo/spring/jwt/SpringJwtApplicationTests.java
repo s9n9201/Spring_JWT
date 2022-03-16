d7456607f2f9c3f7af0867c0249dcc568303d332
kent <s9n9201@yahoo.com.tw>
2022-03-16 10:21:55 +0800

    First Commit

---
+++ src/test/java/demo/spring/jwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.spring.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
