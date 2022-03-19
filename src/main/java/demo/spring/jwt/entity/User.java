de0333669cc7104498a8e8805b0d99b2dd306470
kent <s9n9201@yahoo.com.tw>
2022-03-19 16:01:20 +0800

    add empty Constructor

--- src/main/java/demo/spring/jwt/entity/User.java
+++ src/main/java/demo/spring/jwt/entity/User.java
@@ -31,12 +31,14 @@ public class User {
     @JoinTable(name="user_roles", joinColumns=@JoinColumn(name="user_id"), inverseJoinColumns=@JoinColumn(name="role_id"))
     private Set<Role> roles=new HashSet<>();
 
+    public User() {
+
+    }
     public User(String username, String email, String password) {
         this.username=username;
         this.email=email;
         this.password=password;
     }
-
     public Long getId() {
         return id;
     }
