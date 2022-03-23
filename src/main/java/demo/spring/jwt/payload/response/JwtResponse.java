a449d5da0d7168d3d0aaa91d090d87fc80f98b56
kent <s9n9201@yahoo.com.tw>
2022-03-23 17:10:37 +0800

    modify Argument name

--- src/main/java/demo/spring/jwt/payload/response/JwtResponse.java
+++ src/main/java/demo/spring/jwt/payload/response/JwtResponse.java
@@ -10,8 +10,8 @@ public class JwtResponse {
     private String email;
     private List<String> roles;
 
-    public JwtResponse(String accesToken, Long id, String username, String email, List<String> roles) {
-        this.token=accesToken;
+    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+        this.token=accessToken;
         this.id=id;
         this.username=username;
         this.email=email;
