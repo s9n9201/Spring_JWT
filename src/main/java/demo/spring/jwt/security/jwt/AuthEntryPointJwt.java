80958d1ff2f3ab28f9fb62859d10f75bcaf2e6e3
kent <s9n9201@yahoo.com.tw>
2022-03-24 16:14:37 +0800

    mdify Error word

--- src/main/java/demo/spring/jwt/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/demo/spring/jwt/security/jwt/AuthEntryPointJwt.java
@@ -29,7 +29,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         final Map<String, Object> body=new HashMap<>();
         body.put("status",HttpServletResponse.SC_UNAUTHORIZED);
         body.put("error","Unauthorized");
-        body.put("message",authException.getMessage());
+        body.put("message","帳號密碼錯誤!");
         body.put("path",request.getServletPath());
 
         final ObjectMapper mapper=new ObjectMapper();
