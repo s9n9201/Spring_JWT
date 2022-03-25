ce8c5619b2c19902f2d5833dd61351125d9c4d8b
kent <s9n9201@yahoo.com.tw>
2022-03-25 13:31:20 +0800

    //使用SecurityContextHolder取得請求使用者的資料，這樣Client就不用在Request Entity中，放入使用者資料。

--- src/main/java/demo/spring/jwt/controllers/TestController.java
+++ src/main/java/demo/spring/jwt/controllers/TestController.java
@@ -1,6 +1,8 @@
 package demo.spring.jwt.controllers;
 
+import demo.spring.jwt.security.services.UserDetailsImpl;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,9 @@ public class TestController {
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public String userAccess() {
+        //使用SecurityContextHolder取得請求使用者的資料，這樣Client就不用在Request Entity中，放入使用者資料。
+        UserDetailsImpl UDI=(UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(UDI.getId());
         return "User Content.";
     }
 
