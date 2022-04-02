5271bc6a363ecf84b32762e29b7051cbf0ed2fb0
kent <s9n9201@yahoo.com.tw>
2022-04-02 20:47:33 +0800

    First Commit

---
+++ src/main/java/demo/spring/jwt/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package demo.spring.jwt.repository;
+
+import demo.spring.jwt.entity.RefreshToken;
+import demo.spring.jwt.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    int deleteByUser(User user);
+}
